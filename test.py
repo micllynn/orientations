b7999e58cf7f49424b0be3407b777ebc3bf05c9b
Michael Lynn <micllynn@gmail.com>
2019-07-26 10:12:02 -0400

    Test for Nastaran

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ bar_stim = ori.Bars('stim', p_bar = 0.99, bar_length = 0.8,
 bar_nonstim = ori.Bars('nonstim', p_bar = 0.01, bar_length = 0.8,
     bar_width = 10, angle_probdist = sp_stats.norm,
     angle_dict = {'loc' : 90, 'scale' : 5})
+
+ori.imgbank(bar_stim, bar_nonstim, n_img = 50, folder_name = 'change', )
